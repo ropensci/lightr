a8a88d4e7aed3ef13cf4be91c520160a1f854095
Hugo <hugo.gruson@protonmail.com>
2018-11-16 17:17:31 +0100

    Add non-looped spec2csv

--- R/spec2csv.R
+++ R/spec2csv.R
@@ -1,8 +1,15 @@
-spec2csv <- function(filename) {
+#' @importFrom tools file_path_sans_ext
 
-  data <- switch(fileext,
-                 trm = parse_trm(filename)[[1]],
-                 parse_trm(filename)[[1]])
+spec2csv <- function(filename, overwrite = FALSE) {
 
+  data <- dispatch_parser(filename))[[1]]
+
+  csv_name <- paste0(file_path_sans_ext(filename), ".csv")
+
+  if (file.exists(csv_name) && !overwrite) {
+    stop(csv_name, " already exists. Select `overwrite = TRUE` to overwrite.")
+  }
+
+  write.csv(data, csv_name)
 
 }
