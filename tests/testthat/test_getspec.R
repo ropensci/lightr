aa7b48cb652095e97e6998e15da4a25e09ef3980
Hugo <hugo.gruson@protonmail.com>
2018-11-20 15:45:40 +0100

    Exclude scope spectra from getspec tests

--- tests/testthat/test_getspec.R
+++ tests/testthat/test_getspec.R
@@ -4,8 +4,8 @@ context("metadata")
 test_that("Spectra import", {
 
   res <- getspec(system.file("testdata", package = "lightR"),
-    ext = c("TRM", "ROH", "ProcSpec", "ttt", "trt", "jdx", "jaz", "JazIrrad"))
+    ext = c("TRM", "ProcSpec", "ttt", "jdx", "jaz", "JazIrrad"))
 
-  expect_length(res, 10)
+  expect_length(res, 11)
 
 })
