4e48a9562c3145b4090fc9cc1112ddd6b99b27f8
Hugo Gruson <hugo.gruson@protonmail.com>
2018-11-22 16:09:41 +0100

    Test multicore message on windows

---
+++ tests/testthat/test_multicores.R
@@ -0,0 +1,16 @@
+library(lightR)
+context("multicore")
+
+test_that("Multicores", {
+
+  skip_if(.Platform$OS.type != "windows")
+
+  expect_message(getspec(system.file("testdata", package = "lightR"),
+                         ext = "jdx", cores = 2),
+                 '"cores" set to 1.')
+
+  expect_message(getmetadata(system.file("testdata", package = "lightR"),
+                             ext = "jdx", cores = 2),
+                 '"cores" set to 1.')
+
+})
