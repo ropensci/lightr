29a2f17ef59166c74ee67d66f53e0b09e5176aa3
Hugo Gruson <hugo.gruson@protonmail.com>
2019-02-03 11:42:35 +0100

    Rename functions in multicore test file

--- tests/testthat/test_multicores.R
+++ tests/testthat/test_multicores.R
@@ -4,11 +4,11 @@ test_that("Multicores", {
 
   skip_if(.Platform$OS.type != "windows")
 
-  expect_message(getspec(system.file("testdata", package = "lightr"),
+  expect_message(get_spec(system.file("testdata", package = "lightr"),
                          ext = "jdx", cores = 2),
                  '"cores" set to 1.')
 
-  expect_message(getmetadata(system.file("testdata", package = "lightr"),
+  expect_message(get_metadata(system.file("testdata", package = "lightr"),
                              ext = "jdx", cores = 2),
                  '"cores" set to 1.')
 
