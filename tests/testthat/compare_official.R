a06fad32c2091d141beeb4c79a72054fcdffca1f
Hugo Gruson <hugo.gruson@protonmail.com>
2019-10-24 10:15:15 +0200

    Add comparison to official output to TRM files

--- tests/testthat/compare_official.R
+++ tests/testthat/compare_official.R
@@ -1,7 +1,17 @@
 test_that("compare procspec/spectrasuite", {
 
-  specs <- lr_get_spec(test.file("puffin"), ext = "ProcSpec")
-  specs_spectrasuite <- lr_get_spec(test.file("puffin"), ext = "txt")
+  # Compare result from custom parser (lr_parse_procspec) and official parser
+  specs <- lr_get_spec(test.file("compare"), ext = "ProcSpec")
+  specs_spectrasuite <- lr_get_spec(test.file("compare"), ext = "txt")
 
   expect_equal(specs, specs_spectrasuite, tol = 1e-4)
+})
+
+test_that("compare trm/ttt", {
+
+  # Compare result from custom parser (lr_parse_trm) and official parser
+  specs <- lr_get_spec(test.file("compare"), ext = "TRM")
+  specs_avasoft <- lr_get_spec(test.file("compare"), ext = "ttt")
+
+  expect_equal(specs, specs_avasoft, tol = 1e-4)
 })
