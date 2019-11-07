7974d10a2ea833b000c8b10db5d7844861b14fd1
Hugo Gruson <hugo.gruson@protonmail.com>
2019-11-07 10:43:56 +0100

    Use xml_double instead ox xml_text for numeric values

--- R/parse_procspec.R
+++ R/parse_procspec.R
@@ -6,7 +6,7 @@
 #'
 #' @inherit lr_parse_generic return
 #'
-#' @importFrom xml2 read_xml xml_find_all xml_find_first xml_text
+#' @importFrom xml2 read_xml xml_find_all xml_find_first xml_text xml_double
 #'
 #' @references <https://oceanoptics.com/faq/extract-data-procspec-file-without-spectrasuite/>
 #'
@@ -34,31 +34,30 @@ lr_parse_procspec <- function(filename) {
   # OceanOptics softwares produce badly encoded characters. The only fix is to
   # strip them before feeding the xml file to read_xml.
   plain_text <- scan(data_file, what = character(), sep = "\n", quiet = TRUE)
-  clean_text <- lapply(plain_text, function(line) {
+  clean_text <- vapply(plain_text, function(line) {
     # Convert non-ASCII character to ""
     line <- iconv(line, to = "ASCII", sub = "")
     # Remove the extra malformed character
     line <- gsub("\\\001", "", line)
 
     return(line)
-  })
+  }, character(1))
 
   clean_text <- paste(clean_text, collapse = "\n")
 
   # Because the file is non ASCII, we don't need to specify the encoding.
   xml_source <- read_xml(clean_text)
 
-  wl_node <- xml_find_all(xml_source, ".//channelWavelengths")
-  wl_values <- xml_find_all(wl_node, ".//double")
+  wl_values <- xml_find_all(xml_source, ".//channelWavelengths//double")
   # Get rid of the XML tags.
-  wl <- xml_text(wl_values)
+  wl <- xml_double(wl_values)
 
   scope_node <- xml_find_first(xml_source, ".//pixelValues")
   scope_values <- xml_find_all(scope_node, ".//double")
   scope_inttime <- xml_text(xml_find_first(xml_source, ".//integrationTime"))
   scope_average <- xml_text(xml_find_first(xml_source, ".//scansToAverage"))
   scope_boxcar <- xml_text(xml_find_first(xml_source, ".//boxcarWidth"))
-  scope <- xml_text(scope_values)
+  scope <- xml_double(scope_values)
 
   white_node <- xml_find_all(xml_source, ".//referenceSpectrum")
   white_values <- xml_find_all(white_node, ".//double")
@@ -66,7 +65,7 @@ lr_parse_procspec <- function(filename) {
   white_average <- xml_text(xml_find_all(white_node, ".//scansToAverage"))
   white_boxcar <- xml_text(xml_find_all(white_node, ".//boxcarWidth"))
   # Get rid of the XML tags.
-  white <- xml_text(white_values)
+  white <- xml_double(white_values)
 
   dark_node <- xml_find_all(xml_source, ".//darkSpectrum")
   dark_values <- xml_find_all(dark_node, ".//double")
@@ -74,17 +73,14 @@ lr_parse_procspec <- function(filename) {
   dark_average <- xml_text(xml_find_all(dark_node, ".//scansToAverage"))
   dark_boxcar <- xml_text(xml_find_all(dark_node, ".//boxcarWidth"))
   # Get rid of the XML tags.
-  dark <- xml_text(dark_values)
+  dark <- xml_double(dark_values)
 
   processed_node <- xml_find_all(xml_source, ".//processedPixels")
   processed_values <- xml_find_all(processed_node, ".//double")
   # Get rid of the XML tags.
-  processed <- xml_text(processed_values)
+  processed <- xml_double(processed_values)
 
   specdf <- cbind(wl, dark, white, scope, processed)
-
-  # The XML file was considered as text. So are "wl" and "procspec" columns.
-  specdf <- apply(specdf, 2, as.numeric)
 
   author <- xml_text(xml_find_first(xml_source, ".//userName"))
   savetime <- NA # FIXME
