5282b88e43e03b737bf7e09e648bc66d23f75510
Hugo Gruson <hugo.gruson@protonmail.com>
2020-01-24 14:52:16 +0100

    Remove windows multicore tests

--- tests/testthat/test_multicores.R
+++ tests/testthat/test_multicores.R
@@ -1,17 +1,5 @@
 context("multicore")
 
-test_that("Multicores windows", {
-
-  skip_if(.Platform$OS.type != "windows")
-
-  expect_message(lr_get_spec(test.file(), ext = "jdx", cores = 2),
-                 '"cores" set to 1.')
-
-  expect_message(lr_get_metadata(test.file(), ext = "jdx", cores = 2),
-                 '"cores" set to 1.')
-
-})
-
 test_that("Multicores/single core", {
 
   expect_equivalent(lr_get_spec(test.file(), ext = "jdx", cores = 2),
