2c41acfe6d136b52cdfcd9b9cdd3a6562019ee62
Hugo Gruson <hugo.gruson@protonmail.com>
2020-02-13 16:29:31 +0100

    Compare ProcSpec file to uninterpolated version

--- tests/testthat/compare_official.R
+++ tests/testthat/compare_official.R
@@ -1,8 +1,14 @@
 test_that("compare procspec/spectrasuite", {
 
   # Compare result from custom parser (lr_parse_procspec) and official parser
-  specs <- lr_get_spec(test.file("compare"), ext = "ProcSpec")
-  specs_spectrasuite <- lr_get_spec(test.file("compare"), ext = "txt")
+  specs <- lr_get_spec(test.file("compare/OceanInsight"),
+                       ext = "ProcSpec",
+                       lim = c(100, 1000),
+                       interpolate = FALSE)
+  specs_spectrasuite <- lr_get_spec(test.file("compare/OceanInsight"),
+                                    ext = "txt",
+                                    lim = c(100, 1000),
+                                    interpolate = FALSE)
 
   expect_equal(specs, specs_spectrasuite, tol = 1e-4)
 })
@@ -13,8 +19,10 @@ test_that("compare trm/ttt", {
   # - J_PIR_AVRIL2016_0001 uses Avasoft 6.0
   # - NEW0601 uses Avasoft 7.0
 
-  specs <- lr_get_spec(test.file("compare"), ext = "TRM")
-  specs_avasoft <- lr_get_spec(test.file("compare"), ext = "ttt")
+  # FIXME: for some reason, the conversion from the official parser skips some
+  # wl... Figure out why?
+  specs <- lr_get_spec(test.file("compare/Avantes"), ext = "TRM")
+  specs_avasoft <- lr_get_spec(test.file("compare/Avantes"), ext = "ttt")
 
   expect_equal(specs, specs_avasoft, tol = 1e-4)
 })
