e5b3e969173c02b44b5bea0bf2519e8f8cb4513a
Hugo Gruson <hugo.gruson@protonmail.com>
2020-03-31 13:17:29 +0200

    Use non-existing test file to test deprecation warning This avoids breadcrumbs

--- tests/testthat/test_multicores.R
+++ tests/testthat/test_multicores.R
@@ -2,13 +2,14 @@ context("multicore")
 
 test_that("Multicores/single core", {
 
-  expect_warning(lr_get_spec(test.file(), ext = "jdx", cores = 2),
+  expect_warning(lr_get_spec(test.file(), ext = "test", cores = 2),
                  "deprecated")
 
-  expect_warning(lr_get_metadata(test.file(), ext = "jdx", cores = 2),
+  expect_warning(lr_get_metadata(test.file(), ext = "test", cores = 2),
                  "deprecated")
 
-  expect_warning(lr_convert_tocsv(test.file(), ext = "jdx", cores = 2),
+  # Be careful here because lr_convert_tocsv() leaves breadcrumbs
+  expect_warning(lr_convert_tocsv(test.file(), ext = "test", cores = 2),
                  "deprecated")
 
 })
