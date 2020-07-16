90f042af4fe5a1b06adfbd941053b35ee2674005
Hugo Gruson <hugo.gruson@protonmail.com>
2020-07-16 11:41:38 +0200

    Suppress extra warnings in tests

--- tests/testthat/test_multicores.R
+++ tests/testthat/test_multicores.R
@@ -1,13 +1,19 @@
 test_that("Multicores/single core", {
 
-  expect_warning(lr_get_spec(test.file(), ext = "test", cores = 2),
-                 "deprecated")
+  suppressWarnings(
+    expect_warning(lr_get_spec(test.file(), ext = "test", cores = 2),
+                   "deprecated")
+  )
 
-  expect_warning(lr_get_metadata(test.file(), ext = "test", cores = 2),
-                 "deprecated")
+  suppressWarnings(
+    expect_warning(lr_get_metadata(test.file(), ext = "test", cores = 2),
+                   "deprecated")
+  )
 
-  # Be careful here because lr_convert_tocsv() leaves breadcrumbs
-  expect_warning(lr_convert_tocsv(test.file(), ext = "test", cores = 2),
-                 "deprecated")
+  suppressWarnings(
+    # Be careful here because lr_convert_tocsv() leaves breadcrumbs
+    expect_warning(lr_convert_tocsv(test.file(), ext = "test", cores = 2),
+                   "deprecated")
+  )
 
 })
