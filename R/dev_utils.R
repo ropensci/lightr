4298bca72a42d0181253d4c9efe5a08da372cf88
Hugo Gruson <hugo.gruson@protonmail.com>
2021-03-19 20:05:47 +0100

    Add extra release bullet to release issue

--- R/dev_utils.R
+++ R/dev_utils.R
@@ -2,8 +2,8 @@ test.file <- function(...) {
   system.file("testdata", ..., package = "lightr")
 }
 
-int32_to_uint32 <- function(int32) {
-
-  ifelse(int32>=0, int32, 2^31-1 + int32)
-
+release_bullets <- function() {
+  c(
+    '`rhub::check(platform = "debian-gcc-devel-nold")`'
+  )
 }
