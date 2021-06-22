fb1e3ff2db267a76a9bb783de095f4d91402aae5
Hugo Gruson <hugo.gruson@protonmail.com>
2021-06-22 11:48:12 +0200

    Revert "Use anytime"
    This reverts commit d866c2d13ea9c5a614a72dd918d2d97b23f1ec87.

--- R/parse_oceanoptics_converted.R
+++ R/parse_oceanoptics_converted.R
@@ -31,7 +31,16 @@ lr_parse_jaz <- function(filename) {
   savetime <- grep("^Date: .*", content, value = TRUE)
   savetime <- gsub("^Date: ", "", savetime)
 
-  savetime <- as.character(anytime::anytime(savetime))
+  # OceanOptics files use locale-dependant date formats but it looks like they
+  # are always using English for this, even when the locale is not set to
+  # English
+  orig_locale <- Sys.getlocale("LC_TIME")
+  on.exit(Sys.setlocale("LC_TIME", orig_locale))
+  Sys.setlocale("LC_TIME", "C")
+
+  savetime <- as.character(as.POSIXct(savetime,
+                                      tryFormats = c("%c", "%+"),
+                                      optional = TRUE))
 
   specmodel <- NA_character_
 
