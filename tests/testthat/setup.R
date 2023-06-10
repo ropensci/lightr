b72ee0fd9a97e40af4761fa4ffe502c4263e2ed2
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-06-10 12:52:39 +0200

    Add test case for mixed csv and txt

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -8,4 +8,13 @@ file.copy(
 dir.create(file.path(tdir, "csv"))
 file.rename(file.path(tdir, "spec.csv"), file.path(tdir, "csv", "spec.csv"))
 
-withr::defer(unlink(tdir, recursive = TRUE), teardown_env())
+dir.create(file.path(tempdir(), "csv_mixed"))
+file.copy(
+  c(test.file("spec.csv"), test.file("non_english", "OceanView_nonEN.txt")),
+  file.path(tempdir(), "csv_mixed")
+)
+
+withr::defer(
+  unlink(c(tdir, file.path(tempdir(), "csv_mixed")), recursive = TRUE),
+  teardown_env()
+)
