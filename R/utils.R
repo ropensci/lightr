0b0c9fdb4debdc8e92a1c188e958b18c644a4c1f
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-12-01 13:21:05 +0100

    Add thin tz compatibility wrapper

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,18 @@ int32_to_uint32 <- function(int32) {
   ifelse(int32 >= 0, int32, 2^31 - 1 + int32)
 
 }
+
+convert_backward_tzdata <- function(tz) {
+
+  # Convert timezones removed in tzdata 2024b.
+  # This is a subset of what tzdata-backward is providing, based on cases we
+  # could observe in test files. It can be expanded based on user feedback and
+  # new test files.
+  switch(
+    tz,
+    "EST" = "America/Panama",
+    "CET" = "Europe/Brussels",
+    tz
+  )
+
+}
