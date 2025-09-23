d34a3bf439ef273a365bd68d40ff87eefc74b039
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-09-23 15:21:09 +0200

    Test setting up touchstone (#83)

---
+++ touchstone/script.R
@@ -0,0 +1,34 @@
+# see `help(run_script, package = 'touchstone')` on how to run this
+# interactively
+
+# TODO OPTIONAL Add directories you want to be available in this file or during the
+# benchmarks.
+# touchstone::pin_assets("some/dir")
+
+# installs branches to benchmark
+touchstone::branch_install()
+
+# benchmark a function call from your package (two calls per branch)
+touchstone::benchmark_run(
+  {
+    library(lightr)
+    test.file <- function(...) {
+      system.file("testdata", ..., package = "lightr")
+    }
+  },
+  getspec = lr_get_spec(
+    test.file(),
+    ext = c(
+      "TRM",
+      "ttt",
+      "jdx",
+      "jaz",
+      "JazIrrad",
+      "csv",
+      "txt",
+      "Transmission",
+      "spc"
+    )
+  ),
+  n = 20
+)
