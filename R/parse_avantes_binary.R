15373a3d7c17642bc594aea65b826687f1415014
Hugo Gruson <git@hugogruson.fr>
2026-03-28 14:56:51 +0100

    Revert "Read directly from rawConnection() to limit I/O"
    This reverts commit d158acf379b3018fbf6c3bc198b9820d58b25877.

--- R/parse_avantes_binary.R
+++ R/parse_avantes_binary.R
@@ -35,7 +35,7 @@ lr_parse_avantes_trm <- function(filename, ...) {
 
   # Translation into R by Hugo Gruson
 
-  f <- rawConnection(readBin(filename, "raw", n = file.size(filename)), open = "r")
+  f <- file(filename, "rb")
   on.exit(close(f))
 
   # Header
@@ -244,7 +244,7 @@ lr_parse_avantes_rfl8 <- function(filename, specnum = NULL, ...) {
 lr_parse_avasoft8 <- function(filename, specnum) {
   # File structure information provided courtesy of Avantes
 
-  f <- rawConnection(readBin(filename, "raw", n = file.size(filename)), open = "r")
+  f <- file(filename, "rb")
   on.exit(close(f))
 
   # HEADER
