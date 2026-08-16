a09996c579ea6d54de908060bb861df77b8223df
Hugo Gruson <git@hugogruson.fr>
2026-08-16 10:13:46 +0200

    Use numeric constants instead of calls to nchar

--- R/parse_avantes_converted.R
+++ R/parse_avantes_converted.R
@@ -46,9 +46,10 @@ lr_parse_avantes_ttt <- function(filename, ...) {
   specmodel <- NA_character_
   inttime <- sub("^Integration time: ([[:graph:]]+) ms$", "\\1", content[2])
   average <- sub("^Average: ([[:digit:]]+) scans$", "\\1", content[3])
+  # 34 is the length of "Nr of pixels used for smoothing: " + 1
   boxcar <- substring(
     content[4],
-    nchar("Nr of pixels used for smoothing: ") + 1L
+    34L
   )
 
   # The ID is also sometimes included in the first line (comment line) but not
